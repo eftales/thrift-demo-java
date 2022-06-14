214d966ef4515157305c128b34ea9957c0e5f41f
eftales <1479803084@qq.com>
2022-06-14 17:05:45 +0800

    update:

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,41 @@
+package client;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.*;
+import thrift.ConService;
+
+/**
+ * Created by huyanshi on 2019/03/28.
+ */
+public class Client {
+    private static final String SERVER_IP = "localhost";
+    private static final int SERVER_PORT = 2022;//Thrift server listening port
+
+    private void startClient() {
+        TTransport transport = null;
+        try {
+            transport = new TSocket(SERVER_IP,SERVER_PORT);
+            // 协议要和服务端一致
+            TProtocol protocol = new TBinaryProtocol(transport);
+            ConService.Client client = new ConService.Client(protocol);
+            transport.open();
+            System.out.println(client.getTopo());
+        } catch (TTransportException e) {
+            e.printStackTrace();
+        } catch (TException e) {
+            e.printStackTrace();
+        } finally {
+            if (null != transport) {
+                transport.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.startClient();
+    }
+
+}
---
+++ src/main/java/server/Server.java
@@ -0,0 +1,39 @@
+package server;
+
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.TServerSocket;
+import org.apache.thrift.transport.TServerTransport;
+import org.apache.thrift.transport.TTransportFactory;
+import thrift.ConService;
+
+/**
+ * Created by huyanshi on 2019/03/28.
+ */
+public class Server {
+
+
+    private void startServer() {
+        ConService.Processor processor = new ConService.Processor<ConService.Iface>(new ConServiceImpl());
+        try {
+            TServerTransport transport = new TServerSocket(2022);
+            TThreadPoolServer.Args tArgs = new TThreadPoolServer.Args(transport);
+            tArgs.processor(processor);
+            tArgs.protocolFactory(new TBinaryProtocol.Factory());
+            tArgs.transportFactory(new TTransportFactory());
+            tArgs.minWorkerThreads(10);
+            tArgs.maxWorkerThreads(20);
+            TServer server = new TThreadPoolServer(tArgs);
+            System.out.println("Server started.");
+            server.serve();
+        } catch (Exception e) {
+            System.out.println("thrift服务启动失败");
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.startServer();
+    }
+}
